2b114655a991e2b0b377b110b2a617d22dd482c0
Lameuhton <aurelianeg2000@gmail.com>
2024-08-05 11:45:44 +0200

    added menu (header footer)

--- wp-content/themes/designo/functions.php
+++ wp-content/themes/designo/functions.php
@@ -6,4 +6,16 @@
 
   // for all post types
   add_filter('use_block_editor_for_post_type', '__return_false', 10);
+
+// Menu --------------------------------------------------
+function register_theme_menus() {
+  register_nav_menus(
+    array(
+      'menu-header' => __('Header'),
+      'menu-footer' => __('Footer'),
+    )
+  );
+}
+
+add_action('init', 'register_theme_menus');
 ?>
